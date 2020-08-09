dc821a4f7bab74c00a0cca94a37a1ee1fb9fe905
Harrison Nguyen <nguy2173@umn.edu>
2020-08-09 18:43:14 -0500

    add express-socket.io-session

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 // requires
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
 
