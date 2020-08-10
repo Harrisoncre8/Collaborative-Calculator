1bdcd4aef953036f605d8c047757c1ef47355574
Harrison Nguyen <nguy2173@umn.edu>
2020-08-10 03:20:33 -0500

    server catch and emits calculation object as array

--- server/server.js
+++ server/server.js
@@ -6,16 +6,24 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+// declare empty array in order to spread
+let calcLog = []; 
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Set static folder
 app.use( express.static('client') );
 
 io.on('connection', socket => {
-    console.log('Socket connection established...');
-  })
-
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    // listen for calculation
+    socket.on('calculation', calculation => {
+      // once we catch calculation object, emit it back
+      // to all users on client side as array using spread operator
+      calcLog = [calculation, ...calcLog.slice(0, 9)];
+      io.emit('calcLog', calcLog);
+      calcLog = [];
+    })
+});
 
