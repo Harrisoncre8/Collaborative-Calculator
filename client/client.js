58727e180806ae3e47d0d1ac75a2c7aeb5abbc6e
Harrison Nguyen <nguy2173@umn.edu>
2020-08-09 19:23:47 -0500

    set calculator to clear variables

--- client/client.js
+++ client/client.js
@@ -6,3 +6,45 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousValueTextElement = document.querySelector('[data-previous-value]');
 const currentValueTextElement = document.querySelector('[data-current-value]');
+
+class Calculator {
+  constructor(previousValueTextElement, currentValueTextElement){
+    // set text elements inside Calculator class
+    this.previousValueTextElement = previousValueTextElement;
+    this.currentValueTextElement = currentValueTextElement;
+    this.clear();
+  }
+
+  // function that handles clearing different variables
+  // sets default values
+  clear(){
+    this.currentOperator = '';
+    this.previousOperator = '';
+    this.operation = undefined;
+  }
+
+  // function that handles deleting a single character
+  delete(){
+
+  }
+
+  // update display with current calculation
+  useNumber(number){
+
+  }
+
+  // determine which operator to use
+  useOperator(operator){
+
+  }
+
+  // compute number and display on calculator
+  calculation(){
+
+  }
+
+  // update output values
+  updateDisplay(){
+
+  }
+}
