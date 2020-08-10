459ae04ac721ad8964250aa9b38959eea1e48411
Harrison Nguyen <nguy2173@umn.edu>
2020-08-09 19:08:42 -0500

    declare constant variables

---
+++ client/client.js
@@ -0,0 +1,8 @@
+// constant variables 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousValueTextElement = document.querySelector('[data-previous-value]');
+const currentValueTextElement = document.querySelector('[data-current-value]');
